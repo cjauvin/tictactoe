bcfe1dcbd307ecd29a4e396e1314940e61778d7f
Jacques Joky <jacques.joky@gmail.com>
2019-03-16 09:41:56 -0400

    Fix nested for loop iterator

--- src/index.js
+++ src/index.js
@@ -25,17 +25,13 @@ class Board extends React.Component {
 
     render() {
         const board = [];
-
         for (let i = 0; i < 3; i++) {
             const row = [];
-
-            for (let j = 3 * i; j < 3 * i + 3; j++) {
-                row.push(this.renderSquare(j));
+            for (let j = 0; j < 3; j++) {
+                row.push(this.renderSquare(3 * i + j));
             }
-
             board.push(<div className="board-row">{row}</div>);
         }
-        
         return board;
     }
 }
